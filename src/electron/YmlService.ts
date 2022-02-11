b30ebeed3dca6977545eac9f8f0f16420be27ec0
sigurdmw <sigurdmwahl@gmail.com>
2022-02-11 14:57:51 +0100

    removed and cleaned up some

---
+++ src/electron/YmlService.ts
@@ -0,0 +1,10 @@
+// import yml from "js-yaml"
+// import fs from "fs/promises"
+
+// // Get document, or throw exception on error
+// try {
+//   const doc = yml.load(fs.readFileSync('/home/ixti/example.yml', 'utf8'));
+//   console.log(doc);
+// } catch (e) {
+//   console.log(e);
+// }
