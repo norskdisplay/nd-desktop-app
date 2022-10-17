85f051b40e01cf83a8eef908cda377d978f28b2a
Sigurd Moland Wahl <sigurd.wahl@bouvet.no>
2022-10-17 16:33:39 +0200

    fixed bug

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,19 +1,15 @@
-import CircularProgress from "@mui/material/CircularProgress";
 import { Route, Routes } from "react-router-dom";
 import { Layout } from "./components/Layout";
+import { RouteGuard } from "./components/RouteGuard";
 import { Debug } from "./pages/Debug";
 import { HelpText } from "./pages/HelpText";
 import { Home } from "./pages/Home";
 import { NoMatch } from "./pages/NoMatch";
 import { Settings } from "./pages/Settings";
-import { useUpdatedConfig } from "./utils/useUpdatedConfig";
 
-export const Router = () => {
-	const isLoadingConfig = useUpdatedConfig();
-
-	if (isLoadingConfig) return <CircularProgress color="secondary" />
-	return (
-		<Routes>
+export const Router = () => (
+	<Routes>
+		<Route path="/" element={<RouteGuard />}>
 			<Route path="/" element={<Layout />}>
 				<Route index element={<Home />} />
 				<Route path="index.html" element={<Home />} />
@@ -22,6 +18,6 @@ export const Router = () => {
 				<Route path="settings" element={<Settings />} />
 				<Route path="*" element={<NoMatch />} />
 			</Route>
-		</Routes>
-	)
-}
+		</Route>
+	</Routes>
+)
